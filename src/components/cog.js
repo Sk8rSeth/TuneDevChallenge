130d77a6aef2f08dac750fe02a01d1ca51e5c21d
Sk8rSeth <sethhowell2197@gmail.com>
2020-01-23 16:14:07 -0800

    added menu icon

--- src/components/cog.js
+++ src/components/cog.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import cog from "../images/cog.png"
 import Link from 'gatsby-link';
 
 const Menu = ({toggleTheme}) => (
-    <div className="cogMenu" onClick={toggleTheme}>sort</div>
+    <div className="cogMenu" onClick={toggleTheme}><img src={cog} alt="settings menu" /></div>
 );
 export default Menu;
